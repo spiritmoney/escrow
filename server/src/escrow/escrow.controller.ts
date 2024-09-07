e246403d460c9048dd5b11f5b86bf5bb60f6ecfb
Kingsley Odim <kingodim@outlook.com>
2024-09-07 14:13:09 +0100

    Escrow backend

--- server/src/escrow/escrow.controller.ts
+++ server/src/escrow/escrow.controller.ts
@@ -5,34 +5,52 @@ import { EscrowService } from './escrow.service';
 export class EscrowController {
   constructor(private escrowService: EscrowService) {}
 
+  @Post('new-agreement')
+  async newAgreement(
+    @Body()
+    body: {
+      seller: string;
+      verifier: string;
+      description: string;
+      amount: string;
+    },
+  ) {
+    return this.escrowService.newAgreement(
+      body.seller,
+      body.verifier,
+      body.description,
+      body.amount,
+    );
+  }
+
   @Post('request-delivery/:id')
-  async requestDelivery(@Param('id') id: string) {
+  async requestDelivery(@Param('id') id: number) {
     return this.escrowService.requestDelivery(id);
   }
 
   @Post('confirm-delivery/:id')
-  async confirmDelivery(@Param('id') id: string) {
+  async confirmDelivery(@Param('id') id: number) {
     return this.escrowService.confirmDelivery(id);
   }
 
   @Post('complete/:id')
-  async complete(@Param('id') id: string) {
+  async complete(@Param('id') id: number) {
     return this.escrowService.complete(id);
   }
 
   @Post('refund/:id')
-  async refund(@Param('id') id: string) {
+  async refund(@Param('id') id: number) {
     return this.escrowService.refund(id);
   }
 
   @Post('raise-dispute/:id')
-  async raiseDispute(@Param('id') id: string) {
+  async raiseDispute(@Param('id') id: number) {
     return this.escrowService.raiseDispute(id);
   }
 
   @Post('resolve-dispute/:id')
   async resolveDispute(
-    @Param('id') id: string,
+    @Param('id') id: number,
     @Body('winner') winner: string,
   ) {
     return this.escrowService.resolveDispute(id, winner);
@@ -40,14 +58,14 @@ export class EscrowController {
 
   @Post('set-arbitrator/:id')
   async setArbitrator(
-    @Param('id') id: string,
+    @Param('id') id: number,
     @Body('arbitrator') arbitrator: string,
   ) {
     return this.escrowService.setArbitrator(id, arbitrator);
   }
 
   @Post('claim-timeout/:id')
-  async claimTimeout(@Param('id') id: string) {
+  async claimTimeout(@Param('id') id: number) {
     return this.escrowService.claimTimeout(id);
   }
 }
