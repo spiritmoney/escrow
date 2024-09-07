91526566a97c040757cd63612a1acf09e38780f1
Kingsley Odim <kingodim@outlook.com>
2024-09-07 14:13:09 +0100

    Escrow backend

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EscrowModule } from './escrow/escrow.module';
 
 @Module({
-  imports: [],
+  imports: [EscrowModule],
   controllers: [AppController],
   providers: [AppService],
 })
