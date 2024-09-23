2976d68b58b898daf20e8d4d7c5eadabf3de66a4
Charles Emmanuel <emmaxcharles123@gmail.com>
2024-09-24 00:27:52 +0100

    Changes to be committed: 	modified:   server/src/main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,10 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;  // Default to 3000 if PORT is not set
+  await app.listen(port);
   app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
+
