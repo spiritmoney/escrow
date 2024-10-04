9371eb7f0d6f640fbd86b48705d9562e8942c266
Charles Emmanuel <emmaxcharles123@gmail.com>
2024-10-04 20:26:53 +0100

    Changes to be committed: 	modified:   server/src/users/entities/users.entity.ts

--- server/src/users/entities/users.entity.ts
+++ server/src/users/entities/users.entity.ts
@@ -15,6 +15,8 @@ export const UserSchema = new Schema<User>({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   fullName: { type: String, required: true },
-  isVerified: { type: Boolean, required: true, default: false },
+  isVerified: { type: Boolean, 
+    required: true, 
+    default: false },
   
 }, { timestamps: true });
